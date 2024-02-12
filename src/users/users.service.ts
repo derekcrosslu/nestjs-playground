34e3da8bdabd2c2a39b35cb884db4bebfa68faa0
Derek Cross <derekcrosslu@gmail.com>
2024-02-12 17:13:58 -0500

    adding NotFoundException to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException} from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -14,13 +15,17 @@ export class UsersService {
 
   ];
   findAll(role?:'INTERN' | 'ENGINEER' | 'ADMIN') {
-    if(role){
-      return this.users.filter(user => user.role === role);
+    if (role) {
+      const rolesArray = this.users.filter(user => user.role === role);
+      if (rolesArray.length === 0) throw new NotFoundException(`No user with role ${role} found`);
+      return rolesArray;
     }
     return this.users;
   }
   findOne(id: number) {
-    return this.users.find(user => user.id === id);
+    const user = this.users.find(user => user.id === id);
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    return user
   }
   create(createUserDto: CreateUserDto) {
     const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
