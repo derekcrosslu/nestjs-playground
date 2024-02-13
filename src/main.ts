5434d475ff42fa35de20d736a52d1921ee9c5152
Derek Cross <derekcrosslu@gmail.com>
2024-02-13 13:24:21 -0500

    adding logger, exceptions filter and throtle guard module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+/* eslint-disable prettier/prettier */
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './allexceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost) 
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
+  app.enableCors()
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
