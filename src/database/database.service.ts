8c504e8b546da25218c3700fb920cadc000137a6
Derek Cross <derekcrosslu@gmail.com>
2024-02-12 20:18:44 -0500

    connecting prisma to nestjs, creating employee schema and adding crud for endpoints

---
+++ src/database/database.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
